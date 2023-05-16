f8f19ada7feb95dbc51ecee33c2c37456d1fcfe7
Kyle Crawford <kcraw217@gmail.com>
2023-05-16 14:48:23 -0700

    connect Create to database
    co-authored-by: Sarah Reimann <reimannsarah45@gmail.com>

--- AnimalShelter/Models/Animal.cs
+++ AnimalShelter/Models/Animal.cs
@@ -6,24 +6,24 @@ namespace AnimalShelter.Models
   {
     public int AnimalId { get; set; }
     public string Name { get; set; }
-    public DateOnly DOA { get; set; }
+    public DateTime DOA { get; set; }
     public string Breed { get; set; }
     public string BreedSub { get; set; }
 
-    public Animal(int animalId, string name, DateOnly doa, string breed)
-    {
-      AnimalId = animalId;
-      Name = name;
-      DOA = doa;
-      Breed = breed;
-    }
-    public Animal(int animalId, string name, DateOnly doa, string breed, string breedSub)
-    {
-      AnimalId = animalId;
-      Name = name;
-      DOA = doa;
-      Breed = breed;
-      BreedSub = breedSub;
-    }
+    // public Animal(int animalId, string name, DateTime doa, string breed)
+    // {
+    //   AnimalId = animalId;
+    //   Name = name;
+    //   DOA = doa;
+    //   Breed = breed;
+    // }
+    // public Animal(int animalId, string name, DateTime doa, string breed, string breedSub)
+    // {
+    //   AnimalId = animalId;
+    //   Name = name;
+    //   DOA = doa;
+    //   Breed = breed;
+    //   BreedSub = breedSub;
+    // }
   }
 }
